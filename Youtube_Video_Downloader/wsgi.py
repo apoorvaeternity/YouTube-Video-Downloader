3b8217697d1e6c0c3d2ec75d3d59f55e04f1d7b3
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2018-10-01 13:22:45 +0530

    Update wsgi.py

--- Youtube_Video_Downloader/wsgi.py
+++ Youtube_Video_Downloader/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for Youtube_Video_Downloader project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named  as ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
