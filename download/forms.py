44775e61cda25f8d2ef8cb4c8d08fd33778a0b06
Apoorva Pandey <apoorvapandey365@gmail.com>
2020-10-03 10:45:48 +0530

    Fix clean_url to support share url

--- download/forms.py
+++ download/forms.py
@@ -8,13 +8,17 @@ class DownloadForm(forms.Form):
 
     def clean_url(self):
         url = self.cleaned_data['url']
-        if len(url.split("=")[-1]) != 11:  # Checks whether the url contains the 11 character unique video id
-            raise ValidationError("Incorrect YouTube video URL")
-        # to support mobile YouTube url
-        if 'm.' in url:
-            url = url.replace('m.', '')
         # to support share url
         if 'youtu.be' in url:
             video_id = url.split('/')[-1]
             url = 'https://www.youtube.com/watch?v=' + video_id
+            print(video_id, url)
+
+        # to support mobile YouTube url
+        if 'm.' in url:
+            url = url.replace('m.', '')
+
+        if len(url.split("=")[-1]) != 11:  # Checks whether the url contains the 11 character unique video id
+            raise ValidationError("Incorrect YouTube video URL")
+
         return url
