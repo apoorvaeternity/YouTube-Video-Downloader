31b52b398df32ff168eab056f9eb38ca0b8bd23a
Apoorva Pandey <apoorvapandey365@gmail.com>
2020-10-03 09:37:15 +0530

    Upgrade to django 3 and cleanup views and templates (#8)

--- download/views.py
+++ download/views.py
@@ -1,62 +1,17 @@
 import pafy
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template.defaultfilters import filesizeformat
-from django.views import generic
+from django.views.generic import FormView
+from .forms import DownloadForm
 
 
-def url_corrector(url):
-    # Converts the url to supported format if it's not in a format supported by pafy.
-    correct_url = url
-
-    if 'm.' in url:
-        correct_url = correct_url.replace('m.', '')
-
-    if 'youtu.be' in url:
-        video_id = url.split('/')[-1]
-        correct_url = 'https://www.youtube.com/watch?v=' + video_id
-
-    return correct_url
-
-
-class Download(generic.View):
-    """The view which handles the url and returns the download links."""
-
+class IndexView(FormView):
     template_name = 'download/index.html'
+    form_class = DownloadForm
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
-
-    def post(self, request, *args, **kwargs):
-        url = request.POST['url']
-        url = url_corrector(url)  # Converting the urls in a format that is supported by pafy.
-
-        if len(url.split("=")[-1]) != 11:  # Checks whether the url contains the 11 character unique video id.
-            return HttpResponse('Enter correct url.')
-
-        video = pafy.new(url)  # creates a pafy object for a given youtube url.
-
-        stream = video.streams  # both video and audio
-        video_audio_streams = list()  # list of all the videos which also contain audio
-        for s in stream:
-            video_audio_streams.append(
-                [s.resolution, s.extension, filesizeformat(s.get_filesize()), s.url + "&title=" + video.title])
-
-        stream_video = video.videostreams  # only video
-        video_streams = list()  # list of all the dash video formats(only video)
-        for s in stream_video:
-            video_streams.append(
-                [s.resolution, s.extension, filesizeformat(s.get_filesize()), s.url + "&title=" + video.title])
-
-        stream_audio = video.audiostreams  # only audio
-        audio_streams = list()  # list of all the dash audio formats(only audio)
-        for s in stream_audio:
-            audio_streams.append(
-                [s.resolution, s.extension, filesizeformat(s.get_filesize()), s.url + "&title=" + video.title])
-
-        return render(request, self.template_name,
-                      {'url': request.POST['url'], 'title': video.title, 'streams': video_audio_streams,
-                       'desc': video.description, 'likes': video.likes,
-                       'dislikes': video.dislikes, 'thumb': video.bigthumbhd,
-                       'duration': video.duration, 'views': video.viewcount,
-                       'stream_video': video_streams, 'stream_audio': audio_streams})
+    def form_valid(self, form):
+        """If the form is valid, render to response"""
+        url = form.cleaned_data.get("url")
+        video = pafy.new(url)  # creates a pafy object for a given youtube url
+        video.embed_url = pafy.g.urls["embed"] % (video.videoid,)
+        # insert video object into the context
+        context = self.get_context_data(form=form, video=video)
+        return self.render_to_response(context)
